4a7933879b20a030ea4a9376a04f3aeb1b148c0d
Zhang, Zikai (Data61, Clayton) <Zikai.Zhang@data61.csiro.au>
2020-05-20 00:19:29 +1000

    init

---
+++ precache-manifest.10ee997c434ae5b6617b56b133f7a2e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9be60163352fe0361148a65adb04f52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6387fee012b95e2e7d1a",
+    "url": "/static/css/2.35013e12.chunk.css"
+  },
+  {
+    "revision": "615106cf0bcb6a2ec01b",
+    "url": "/static/css/main.ec4874b8.chunk.css"
+  },
+  {
+    "revision": "6387fee012b95e2e7d1a",
+    "url": "/static/js/2.969168e8.chunk.js"
+  },
+  {
+    "revision": "89c877764be78d03d37f0f88f277a561",
+    "url": "/static/js/2.969168e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "615106cf0bcb6a2ec01b",
+    "url": "/static/js/main.0bb0b563.chunk.js"
+  },
+  {
+    "revision": "37740c5fe0df1186d278",
+    "url": "/static/js/runtime-main.16d0157d.js"
+  }
+]);
